ec5e7384bd5793faf511d4201f4d1173a649be3c
rakesh <rxb162130@utdallas.edu>
2017-09-12 16:44:19 -0500

    Completed decimal to base n conversion

--- LongProjects/LP1/src/cs6301/g38/LP1L1.java
+++ LongProjects/LP1/src/cs6301/g38/LP1L1.java
@@ -3,14 +3,23 @@
 // Change following line to your group number
 package cs6301.g38;
 
+import java.util.Scanner;
+
 public class LP1L1 {
     public static void main(String[] args) {
-	Num x = new Num(999);
-	Num y = new Num("8");
-	Num z = Num.add(x, y);
-	System.out.println(z);
-	Num a = Num.power(x, 8);
-	System.out.println(a);
-	z.printList();
+    	Scanner in =new Scanner(System.in);
+    	Num y;
+    	System.out.println("Enter Big Number");
+    	y = new Num(in.next());
+    	System.out.println("Enter base");
+    	y.setBase(in.nextLong());
+    	y.toBase();
+    	y.printList();
+    	in.close();
+	//Num z = Num.add(x, y);
+	//System.out.println(z);
+	//Num a = Num.power(x, 8);
+	//System.out.println(a);
+	//z.printList();
     }
 }
--- LongProjects/LP1/src/cs6301/g38/Num.java
+++ LongProjects/LP1/src/cs6301/g38/Num.java
@@ -5,6 +5,8 @@
 // Changed type of base to long: 1:15 PM, 2017-09-08.
 package cs6301.g38;
 
+import java.util.LinkedList;
+
 /**
  * @author Rajkumar PanneerSelvam - rxp162130 <br>
  *         Avinash Venkatesh - axv165330 <br>
@@ -15,15 +17,66 @@ package cs6301.g38;
 public class Num  implements Comparable<Num> {
 
     static long defaultBase = 10;  // This can be changed to what you want it to be.
-    long base = defaultBase;  // Change as needed
+    long base;  // Change as needed
+    public long getBase() {
+		return base;
+	}
 
+	public void setBase(long base) {
+		this.base = base;
+	}
+
+	LinkedList<String> num = new LinkedList<>();
+    
     /* Start of Level 1 */
     Num(String s) {
+    	String [] temp = s.split("");
+    	for(String i:temp) {
+    		num.addFirst(i);
+    	}
     }
 
     Num(long x) {
+    	
+    	String str = Long.toString(x);
+    	String[] temp = str.split("");
+    	for(String i:temp) {
+    		num.addFirst(i);
+    	}
     }
-
+    public void toBase() {
+    	LinkedList<String> quotient = new LinkedList<String>();
+    	LinkedList<String> reminder = new LinkedList<String>();
+    	while(!num.isEmpty()){
+    		quotient.clear();
+    		int i=num.size()-1;
+    		long temp = Long.parseLong(num.get(i--));
+    		while(i>=0) {
+    	    	  quotient.addFirst(Long.toString(temp/base));
+    	    	  temp = temp%base;
+    	    	  if(temp<base&&i>=0)
+    	    		  temp=temp*10+Long.parseLong(num.get(i--));
+    	    }
+    		quotient.addFirst(Long.toString(temp/base));
+    		num.clear();
+    		while(!quotient.isEmpty()&&quotient.getLast().equals(new String("0")))
+    			quotient.removeLast();
+    		if(!quotient.isEmpty())
+    		for(String x :quotient) {
+    			num.add(x);
+    		}
+    		reminder.add(Long.toString(temp%base));
+    	}
+    	for(String i:reminder) {
+    		num.add(i);
+    	}
+    }
+    public void toDecimal() {
+    	int p=0;
+    	for(String i:num) {
+    		Long.parseLong(i);
+    	}
+    }
     static Num add(Num a, Num b) {
 	return null;
     }
@@ -73,6 +126,9 @@ public class Num  implements Comparable<Num> {
     // For example, if base=100, and the number stored corresponds to 10965,
     // then the output is "100: 65 9 1"
     void printList() {
+    	for(String i:num) {
+    		System.out.print(i+" ");
+    	}
     }
     
     // Return number to a string in base 10
