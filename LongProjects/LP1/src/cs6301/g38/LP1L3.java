2354019417694f596606d3f32ddd17ad302754ec
rakesh <rxb162130@utdallas.edu>
2017-09-21 17:18:54 -0500

    LP1 Level 3 completed and ready for testing.

--- LongProjects/LP1/src/cs6301/g38/LP1L3.java
+++ LongProjects/LP1/src/cs6301/g38/LP1L3.java
@@ -8,11 +8,16 @@ public class LP1L3 {
 
        public static void main(String[] args) {
 	Scanner in = new Scanner(System.in);
-	LP1L3 x = new LP1L3();
-	
+	//LP1L3 x = new LP1L3();
+	Expression e = new Expression();
+	String word;
 	while(in.hasNext()) {
-	    String word = in.next();
-	    // To do ...
+	    word = in.nextLine();
+	    if(word.equals(";")) {
+	    	break;
+	    }
+	    e.eval(word);
 	}
+	in.close();
     }
 }
