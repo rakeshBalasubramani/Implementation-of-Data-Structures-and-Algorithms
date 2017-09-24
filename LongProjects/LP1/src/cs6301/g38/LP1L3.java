dcc8b342395e9da8a341468f261892d45040fdaa
haripriyaa <hum160030@utdallas.edu>
2017-09-24 13:11:34 -0500

    level 3 test cases so far

--- LongProjects/LP1/src/cs6301/g38/LP1L3.java
+++ LongProjects/LP1/src/cs6301/g38/LP1L3.java
@@ -3,12 +3,21 @@
 // Change following line to your group number
 package cs6301.g38;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class LP1L3 {
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner in;
+		if (args.length > 0) {
+		    File inputFile = new File(args[0]);
+		    in = new Scanner(inputFile);
+		} else {
+		    in = new Scanner(System.in);
+		}
+	
 		// LP1L3 x = new LP1L3();
 		Expression e = new Expression();
 		String word;
