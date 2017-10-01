362d82d1e92a313d850880d1e6fbb1d574335726
esaabdk <rajkumarp1591@gmail.com>
2017-10-01 16:58:43 -0500

    SP5 : separate file for Selection comparison

--- ShortProjects/SP5/src/cs6301/g38/PriorityQueueSelection.java
+++ ShortProjects/SP5/src/cs6301/g38/PriorityQueueSelection.java
@@ -3,8 +3,6 @@ package cs6301.g38;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.PriorityQueue;
-import java.util.Random;
-import java.util.Scanner;
 
 /**
  * @author Rajkumar PanneerSelvam - rxp162130 <br>
@@ -150,63 +148,5 @@ public class PriorityQueueSelection<T> {
 		return result;
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner in = new Scanner(System.in);
-		System.out.println("Enter the array size in millions");
-
-		int size = in.nextInt();
-int million= 1000000;
-size = size * million;
-		System.out.println("Enter the value of k :");
-		int k = in.nextInt();
-		Integer arr[] = new Integer[size];
-		Random rand = new Random();
-		for (int i = 0; i < size; i++)
-			arr[i] = (rand.nextInt(i + 1));
-
-//		System.out.println("Input array :");
-//		for (int i : arr) {
-//			System.out.print(i + " ");
-//		}
-		System.out.println();
-
-		Timer time = new Timer();
-		time.start();
-		Integer r[] = maxHeapSelectAlgo(arr, k);
-		time.end();
-		System.out.println("----------------------------------");
-		System.out.println("Selection using Max heap");
-		System.out.println(time);
-
-//		for (int i : r) {
-//			System.out.print(i + " ");
-//		}
-System.out.println();
-		time.start();
-		r = minHeapSelectAlgo(arr, k);
-		time.end();
-		System.out.println("----------------------------------");
-
-		System.out.println("Selection using Min heap");
-		System.out.println(time);
-
-//		for (int i : r) {
-//			System.out.print(i + " ");
-//		}
-System.out.println();
-		time.start();
-		r = LinearTimeSelection.select(arr, k);
-		time.end();
-		System.out.println("----------------------------------");
-
-		System.out.println("Linear time selection");
-		System.out.println(time);
-
-//		for (int i : r) {
-//			System.out.print(i + " ");
-//		}
-
-		in.close();
-	}
+	
 }
