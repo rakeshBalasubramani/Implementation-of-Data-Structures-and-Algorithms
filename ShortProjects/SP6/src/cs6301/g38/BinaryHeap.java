ba514b3eafb8edb8ad1afe7aec8eeef64420d464
esaabdk <rajkumarp1591@gmail.com>
2017-10-06 00:44:09 -0500

    SP6: BinaryHeap Impl : intial version

--- ShortProjects/SP6/src/cs6301/g38/BinaryHeap.java
+++ ShortProjects/SP6/src/cs6301/g38/BinaryHeap.java
@@ -6,6 +6,7 @@ import java.util.Comparator;
 public class BinaryHeap<T> {
     T[] pq;
     Comparator<T> c;
+    int size;
     /** Build a priority queue with a given array q, using q[0..n-1].
      *  It is not necessary that n == q.length.
      *  Extra space available can be used to add new elements.
@@ -13,9 +14,10 @@ public class BinaryHeap<T> {
     public BinaryHeap(T[] q, Comparator<T> comp, int n) {
 	pq = q;
 	c = comp;
+	size=n-1;
     }
 
-    public void insert(T x) {
+    public void insert(T x) throws Exception {
 	add(x);
     }
 
@@ -27,15 +29,47 @@ public class BinaryHeap<T> {
 	return peek();
     }
 
-    public void add(T x) { /* TO DO. Throw exception if q is full. */
+    public void add(T x) throws Exception { /* TO DO. Throw exception if q is full. */
+    	if(size==pq.length)
+    	{
+    		throw new Exception( " Queue is Full");
+    	}
+    	
+    	pq[size] = x ;
+    	 percolateUp(size);
+    	 size++;
     }
 
     public T remove() { /* TO DO. Throw exception if q is empty. */
-	return null;
+    	
+    	if(size < 0)
+    	{
+    		return null;
+    	}
+    	T min = pq[0];
+    	if (size ==0)
+    	{
+    		size--;
+    	}
+    	else
+    	{
+        move(0,pq[size--]);//	pq[0]= pq[size--];
+        	percolateDown(0);
+    	}
+	return min;
     }
 
     public T peek() { /* TO DO. Throw exception if q is empty. */
-	return null;
+    	
+    	// handle empty case
+    	if(size < 0)
+    	{
+    		return null;
+    	}
+    	
+    	return pq[0];
+    
+	
     }
 
     public void replace(T x) {
@@ -43,19 +77,78 @@ public class BinaryHeap<T> {
 	     (smaller) than root, and restore heap order.  Otherwise do nothing. 
 	   This operation is used in finding largest k elements in a stream.
 	 */
+    	if(size < 0)
+    	{
+    		pq[0]=x;
+    		size++;
+    	}
+    	
+    	if(c.compare(x, peek())<0)
+    	{
+    		pq[0]=x;
+    		percolateDown(0);
+    	}
     }
 
     /** pq[i] may violate heap order with parent */
     void percolateUp(int i) { /* to be implemented */
+    	
+    	T x = pq[i];
+    	
+    	while(i>0 && c.compare(x, pq[parent(i)])<0)
+    	{
+    		move(i,pq[parent(i)]);//pq[i] = pq[parent(i)];
+    		i = parent(i);
+    	}
+    	
+    move(i,x);//	pq[i] = x;
+    	
+    	
     }
 
     /** pq[i] may violate heap order with children */
     void percolateDown(int i) { /* to be implemented */
-    }
+    	
+    	T x = pq[i];
+    	int child = 2*i+1;
+    	while(child<=size-1)
+    	{
+    		if(child<size-1 && c.compare(pq[child], pq[child+1])>0)
+    		{
+    			child++;
+    		}
+    		if(c.compare(x, pq[child])<=0)
+    		{
+    	break;
+    		}
+    		move(i,pq[child]);//	pq[i]= pq[child];
+			i = child;
+			child= 2*i+1;
+    		
+    	}
+    
+    	move(i,x);//pq[i]=x;
+    	}
 
     /** Create a heap.  Precondition: none. */
     void buildHeap() {
+    	
+    	for(int i = parent(size-1); i>=0; i--)
+    	{
+    		percolateDown(i);
+    	}
     }
+    
+     int parent( int child)
+    {
+    	return (child-1)/2;
+    }
+     
+     void move(int i, T x)
+     {
+    	
+    	 pq[i]=x;
+     }
 
     /* sort array A[].
        Sorted order depends on comparator used to buid heap.
@@ -63,5 +156,40 @@ public class BinaryHeap<T> {
        max heap ==> ascending order
      */
     public static<T> void heapSort(T[] A, Comparator<T> comp) { /* to be implemented */
+    	
+    	BinaryHeap<T> heap = new BinaryHeap<T>(A, comp, A.length);
+    	
+    	heap.buildHeap();
+    	int len = A.length-1;
+    	while(heap.peek()!=null)
+    	{
+    		A[len]=(heap.remove());
+    		len--;
+    	}
+    	
+    	for(T va: A)
+    	{
+    		System.out.println(va);
+    	}
+    	
+    	
+    }
+    
+    
+    public static void main(String args[])
+    {
+    	Integer []a ={7,3,2,8,4,6,9,1,5};
+    	
+    	Comparator<Integer> c = new Comparator<Integer>() {
+
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				// TODO Auto-generated method stub
+				return -1*Integer.compare(o1, o2);
+			}
+    		
+		};
+    	
+    	BinaryHeap.heapSort(a, c);
     }
 }
