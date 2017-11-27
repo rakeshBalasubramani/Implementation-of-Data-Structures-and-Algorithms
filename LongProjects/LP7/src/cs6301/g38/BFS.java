51c853b998670a9e8ef2c09d1adc742d4743c284
esaabdk <rajkumarp1591@gmail.com>
2017-11-26 22:45:31 -0600

    LP7: flow verified

--- LongProjects/LP7/src/cs6301/g38/BFS.java
+++ LongProjects/LP7/src/cs6301/g38/BFS.java
@@ -1,4 +1,5 @@
 package cs6301.g38;
+
 /** Breadth-first search
  *  @author rbk
  *  Version 1.0: 2017/09/08
@@ -9,92 +10,91 @@ import java.util.LinkedList;
 
 import cs6301.g38.MaxFlowGraph.FVertex;
 
-
-
 public class BFS extends GraphAlgorithm<BFS.BFSVertex> {
-    public static final int INFINITY = Integer.MAX_VALUE;
-    private MaxFlowGraph graph;
-    // Class to store information about a vertex in this algorithm
-    static class BFSVertex {
-	boolean seen;
-	Graph.Vertex parent;
-	int distance;  // distance of vertex from source
-	BFSVertex(Graph.Vertex u) {
-	    seen = false;
-	    parent = null;
-	    distance = INFINITY;
-	}
-	@Override
-	public String toString() {
-		return "BFSVertex [seen=" + seen + ", parent=" + parent + ", distance="
-				+ distance + "]";
-	}
-	
-	
-    }
+	public static final int INFINITY = Integer.MAX_VALUE;
+	private MaxFlowGraph graph;
 
-    Graph.Vertex src;
+	// Class to store information about a vertex in this algorithm
+	static class BFSVertex {
+		boolean seen;
+		Graph.Vertex parent;
+		int distance; // distance of vertex from source
 
-    public BFS(Graph g, Graph.Vertex src) {
-	super(g);
-	graph = (MaxFlowGraph) g;
-	this.src = src;
-	node = new BFSVertex[g.size()];
-	// Create array for storing vertex properties
-	for(Graph.Vertex u: g) {
-	    node[u.getName()] = new BFSVertex(u);
-	}
-	// Set source to be at distance 0
-	getVertex(src).distance = 0;
-    }
-
-    // reinitialize allows running BFS many times, with different sources
-    void reinitialize(Graph.Vertex newSource) {
-	src = newSource;
-	for(Graph.Vertex u: g) {
-	    BFSVertex bu = getVertex(u);
-	    bu.seen = false;
-	    bu.parent = null;
-	    bu.distance = INFINITY;
-	}
-	getVertex(src).distance = 0;
-    }
-
-    void bfs() {
-	Queue<FVertex> q = new LinkedList<>();
-	q.add(graph.getVertex(src));
-	getVertex(src).seen=true;
-	while(!q.isEmpty()) {
-	    Graph.Vertex u = q.remove();
-	    FVertex fv = (FVertex) u;
-	    for(Graph.Edge e: fv) {
-		Graph.Vertex v = e.otherEnd(u);
-		if(!seen(v)) {
-		    visit(u,v);
-		    q.add(graph.getVertex(v));
+		BFSVertex(Graph.Vertex u) {
+			seen = false;
+			parent = null;
+			distance = INFINITY;
 		}
-	    }
+
+		@Override
+		public String toString() {
+			return "BFSVertex [seen=" + seen + ", parent=" + parent
+					+ ", distance=" + distance + "]";
+		}
+
 	}
-    }
-    
-    boolean seen(Graph.Vertex u) {
-	return getVertex(u).seen;
-    }
 
-    Graph.Vertex getParent(Graph.Vertex u) {
-	return getVertex(u).parent;
-    }
+	Graph.Vertex src;
 
-    int distance(Graph.Vertex u) {
-	return getVertex(u).distance;
-    }
+	public BFS(Graph g, Graph.Vertex src) {
+		super(g);
+		graph = (MaxFlowGraph) g;
+		this.src = src;
+		node = new BFSVertex[g.size()];
+		// Create array for storing vertex properties
+		for (Graph.Vertex u : g) {
+			node[u.getName()] = new BFSVertex(u);
+		}
+		// Set source to be at distance 0
+		getVertex(src).distance = 0;
+	}
 
+	// reinitialize allows running BFS many times, with different sources
+	void reinitialize(Graph.Vertex newSource) {
+		src = newSource;
+		for (Graph.Vertex u : g) {
+			BFSVertex bu = getVertex(u);
+			bu.seen = false;
+			bu.parent = null;
+			bu.distance = INFINITY;
+		}
+		getVertex(src).distance = 0;
+	}
 
-    // Visit a node v from u
-    void visit(Graph.Vertex u, Graph.Vertex v) {
-	BFSVertex bv = getVertex(v);
-	bv.seen = true;
-	bv.parent = u;
-	bv.distance = distance(u) + 1;
-    }
+	void bfs() {
+		Queue<FVertex> q = new LinkedList<>();
+		q.add(graph.getVertex(src));
+		getVertex(src).seen = true;
+		while (!q.isEmpty()) {
+			Graph.Vertex u = q.remove();
+			FVertex fv = (FVertex) u;
+			for (Graph.Edge e : fv) {
+				Graph.Vertex v = e.otherEnd(u);
+				if (!seen(v)) {
+					visit(u, v);
+					q.add(graph.getVertex(v));
+				}
+			}
+		}
+	}
+
+	boolean seen(Graph.Vertex u) {
+		return getVertex(u).seen;
+	}
+
+	Graph.Vertex getParent(Graph.Vertex u) {
+		return getVertex(u).parent;
+	}
+
+	int distance(Graph.Vertex u) {
+		return getVertex(u).distance;
+	}
+
+	// Visit a node v from u
+	void visit(Graph.Vertex u, Graph.Vertex v) {
+		BFSVertex bv = getVertex(v);
+		bv.seen = true;
+		bv.parent = u;
+		bv.distance = distance(u) + 1;
+	}
 }
